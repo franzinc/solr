5ca7cc468ab616aaf823f00dc7e1fa23c6a9cd4c
Kevin Layer <layer@franz.com>
2013-01-31 10:47:32 -0800

    Change copyright to Eclipse

--- package.lisp
+++ package.lisp
@@ -1,4 +1,9 @@
 ;; -*- mode: common-lisp -*-
+;; copyright (c) 2011-2013 Franz Inc, Oakland, CA - All rights reserved.
+;; This program and the accompanying materialsare made available under the
+;; terms of the Eclipse Public License v1.0 which accompanies this
+;; distribution (see license.txt), and is available at
+;;   http://www.eclipse.org/legal/epl-v10.html
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require :aserve)
--- solr.lisp
+++ solr.lisp
@@ -1,19 +1,9 @@
 ;; -*- mode: common-lisp -*-
 ;; copyright (c) 2011-2013 Franz Inc, Oakland, CA - All rights reserved.
-;;
-;; The software, data and information contained herein are proprietary
-;; to, and comprise valuable trade secrets of, Franz, Inc.  They are
-;; given in confidence by Franz, Inc. pursuant to a written license
-;; agreement, and may be stored and used only in accordance with the terms
-;; of such license.
-;;
-;; Restricted Rights Legend
-;; ------------------------
-;; Use, duplication, and disclosure of the software, data and information
-;; contained herein by any agency, department or entity of the U.S.
-;; Government are subject to restrictions of Restricted Rights for
-;; Commercial Software developed at private expense as specified in
-;; DOD FAR Supplement 52.227-7013 (c) (1) (ii), as applicable.
+;; This program and the accompanying materialsare made available under the
+;; terms of the Eclipse Public License v1.0 which accompanies this
+;; distribution (see license.txt), and is available at
+;;   http://www.eclipse.org/legal/epl-v10.html
 
 (in-package :solr)
 
